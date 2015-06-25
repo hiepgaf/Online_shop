6ff9196451e6b0ef3d7186eade31b7a105f84cdc
Eugen Kostko <Zhenjka25@mail.ru>
2015-06-25 05:28:13 +0200

    commit 37

--- shop/src/main/java/by/epam/shop/filter/ServletSecurityFilter.java
+++ shop/src/main/java/by/epam/shop/filter/ServletSecurityFilter.java
@@ -5,37 +5,28 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import by.epam.shop.entity.User;
-
-@WebFilter(urlPatterns = { "/pages/*" })
+@WebFilter(urlPatterns = { "/pages/*" }, initParams = { @WebInitParam(name = "INDEX_PATH", value = "/index.jsp") })
 public class ServletSecurityFilter implements Filter {
+	private String indexPath;
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
+		indexPath = filterConfig.getInitParameter("INDEX_PATH");
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse resp = (HttpServletResponse) response;
-		HttpSession session = req.getSession();
-		User user = (User) session.getAttribute("user");
-		if (user == null) {
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher("/pages/main.jsp");
-			dispatcher.forward(req, resp);
-			return;
-		}
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.sendRedirect(httpRequest.getContextPath() + indexPath);
 		chain.doFilter(request, response);
 	}
 
