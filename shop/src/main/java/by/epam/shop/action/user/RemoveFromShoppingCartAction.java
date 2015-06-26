ebe33738fcc33db3455ad53fbdd3a7280a4459a9
ugt183 <Zhenjka25@mail.ru>
2015-06-26 13:16:37 +0200

    commit 43

--- shop/src/main/java/by/epam/shop/action/user/RemoveFromShoppingCartAction.java
+++ shop/src/main/java/by/epam/shop/action/user/RemoveFromShoppingCartAction.java
@@ -1,10 +1,11 @@
 package by.epam.shop.action.user;
 
+import java.util.Iterator;
+
 import javax.servlet.http.HttpServletRequest;
 
 import by.epam.shop.action.Action;
 import by.epam.shop.constant.MessageKeys;
-import by.epam.shop.dao.ProductDAO;
 import by.epam.shop.entity.Product;
 import by.epam.shop.entity.User;
 
@@ -22,13 +23,12 @@ public class RemoveFromShoppingCartAction implements Action {
 			return configurationManager.getProperty("path.page.error");
 		}
 		int id = Integer.parseInt(request.getParameter("product_id"));
-		ProductDAO productDAO = new ProductDAO();
-		Product product = productDAO.findEntityById(id);
-		if (product == null) {
-			request.setAttribute("message", MessageKeys.FIND_PRODUCT_ERROR);
-			return configurationManager.getProperty("path.page.error");
+		Iterator<Product> iterator = user.getShoooppingCart().iterator();
+		while (iterator.hasNext()) {
+			if (iterator.next().getId() == id) {
+				iterator.remove();
+			}
 		}
-		user.removeProduct(product);
 		request.getSession().setAttribute("user", user);
 		request.setAttribute("message",
 				MessageKeys.REMOVE_FROM_SHOPPING_CART_SUCCESS);
